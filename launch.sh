971c6be5694891f8d9eab5d59cb2f469a36d2a90
Jack <jack@void.gateway>
2018-04-15 13:28:00 +1000

    Updated configs to my liking

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@ killall -q wallpaperchanger
 # Wait until the processes have been shut down
 while pgrep -x wallpaperchanger >/dev/null; do sleep 1; done
 
-wallpaperchanger ~/Pictures/Wallpapers/wallpapers ~/.config/wall.png 60 85
+wallpaperchanger ~/Pictures/desktop/ ~/.config/wall.png 300 85
